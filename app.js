a9a5f65ed20b2cce903d2a92248875fbb4638220
SarunyuRuenruay <sarunyu.n17@gmail.com>
2020-11-07 01:04:03 +0700

    login path

--- app.js
+++ app.js
@@ -2,10 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookie = require('cookie-parser');
 const app = express();
-app.use(cookie())
+
+app.use(cookie());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.set('view engine', 'ejs');// set view
-app.get('/', (req, res) => {
+
+app.get('/', function(req, res) {
     res.render('index.ejs')
 });
+app.get('/login', function(req, res){
+    res.sendFile(__dirname+"/"+"loginForm.html");
+});
+
+app.post('/auth', function(req, res){
+    data = {
+        username: req.body.user,
+        password: req.body.password
+    };
+    console.log(data);
+    res.end(JSON.stringify(data));
+
+});
 //app.post()
-app.listen(8081)
+app.listen(80)
